10fdf18603092630d50f40bf02e0084a7de7176d
Muhammed Bacalan <muhammedbacalan@gmail.com>
2023-03-04 14:46:32 +0100

    Pull year info from struct

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,42 @@
 use chrono::{Datelike, DateTime, Duration, Utc, NaiveDate};
 
 struct YearProgress {
-    now: DateTime<Utc>,
+    datetime: DateTime<Utc>,
+    year: i32
 }
 
 impl YearProgress {
     pub fn new(now: DateTime<Utc>) -> YearProgress {
-        YearProgress { now }
+        YearProgress {
+            datetime: now,
+            year: now.year()
+        }
     }
 
-    fn get_year_start(&self, year: i32) -> Option<NaiveDate> {
-        NaiveDate::from_ymd_opt(year, 1, 1)
+    fn get_year_start(&self) -> Option<NaiveDate> {
+        NaiveDate::from_ymd_opt(self.year, 1, 1)
     }
 
-    fn get_year_end(&self, year: i32) -> Option<NaiveDate> {
-        NaiveDate::from_ymd_opt(year, 12, 31)
+    fn get_year_end(&self) -> Option<NaiveDate> {
+        NaiveDate::from_ymd_opt(self.year, 12, 31)
     }
 
-    fn get_year_duration(&self, year: i32) -> Duration {
-        let year_start = self.get_year_start(year).unwrap();
-        let year_end = self.get_year_end(year).unwrap();
+    fn get_year_duration(&self) -> Duration {
+        let year_start = self.get_year_start().unwrap();
+        let year_end = self.get_year_end().unwrap();
 
         year_end - year_start
     }
 
-    fn get_elapsed_duration(&self, year: i32) -> Duration {
-        let year_start = self.get_year_start(year).unwrap();
+    fn get_elapsed_duration(&self) -> Duration {
+        let year_start = self.get_year_start().unwrap();
 
-        self.now.date_naive() - year_start
+        self.datetime.date_naive() - year_start
     }
 
     pub fn get(self) -> f64 {
-        let year_duration = self.get_year_duration(self.now.year()).num_seconds();
-        let elapsed_duration = self.get_elapsed_duration(self.now.year()).num_seconds();
+        let year_duration = self.get_year_duration().num_seconds();
+        let elapsed_duration = self.get_elapsed_duration().num_seconds();
 
         elapsed_duration as f64 / year_duration as f64
     }
