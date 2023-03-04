95e81b1e710772fbdd4b47b06243f219be9bc66b
Muhammed Bacalan <muhammedbacalan@gmail.com>
2023-03-04 14:40:51 +0100

    Simplify operations

--- src/main.rs
+++ src/main.rs
@@ -18,30 +18,30 @@ impl YearProgress {
     }
 
     fn get_year_duration(&self, year: i32) -> Duration {
-        let year_start = self.get_year_start(year);
-        let year_end = self.get_year_end(year);
+        let year_start = self.get_year_start(year).unwrap();
+        let year_end = self.get_year_end(year).unwrap();
 
-        year_end.unwrap() - year_start.unwrap()
+        year_end - year_start
     }
 
     fn get_elapsed_duration(&self, year: i32) -> Duration {
-        let year_start = self.get_year_start(year);
+        let year_start = self.get_year_start(year).unwrap();
 
-        self.now.date_naive() - year_start.unwrap()
+        self.now.date_naive() - year_start
     }
 
     pub fn get(self) -> f64 {
-        let year_duration = self.get_year_duration(self.now.year());
-        let elapsed_duration = self.get_elapsed_duration(self.now.year());
+        let year_duration = self.get_year_duration(self.now.year()).num_seconds();
+        let elapsed_duration = self.get_elapsed_duration(self.now.year()).num_seconds();
 
-        elapsed_duration.num_seconds() as f64 / year_duration.num_seconds() as f64
+        elapsed_duration as f64 / year_duration as f64
     }
 }
 
 fn main() {
-    let year_progress = YearProgress::new(Utc::now());
+    let year_progress = YearProgress::new(Utc::now()).get();
 
-    println!("Year Progress: {:.2}%", year_progress.get() * 100.0);
+    println!("Year Progress: {:.2}%", year_progress * 100.0);
 }
 
 #[cfg(test)]
@@ -51,13 +51,13 @@ mod tests {
 
     #[test]
     fn past_year_progress() {
-        let year_progress = YearProgress::new(Utc.with_ymd_and_hms(2022, 12, 31, 0, 0, 0).unwrap());
-        assert_eq!(year_progress.get() * 100.0, 100.00);
+        let year_progress = YearProgress::new(Utc.with_ymd_and_hms(2022, 12, 31, 0, 0, 0).unwrap()).get();
+        assert_eq!(year_progress * 100.0, 100.00);
     }
 
     #[test]
     fn future_year_progress() {
-        let year_progress = YearProgress::new(Utc.with_ymd_and_hms(2030, 1, 1, 0, 0, 0).unwrap());
-        assert_eq!(year_progress.get() * 100.0, 0.00);
+        let year_progress = YearProgress::new(Utc.with_ymd_and_hms(2030, 1, 1, 0, 0, 0).unwrap()).get();
+        assert_eq!(year_progress * 100.0, 0.00);
     }
 }
