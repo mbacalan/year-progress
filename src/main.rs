be5dfda8d20bfe55813f0eaae8f534b8c0969fe0
Muhammed Bacalan <muhammedbacalan@gmail.com>
2023-03-15 21:25:14 +0100

    Remove unwraps

--- src/main.rs
+++ src/main.rs
@@ -21,47 +21,67 @@ impl YearProgress {
         NaiveDate::from_ymd_opt(self.year, 12, 31)
     }
 
-    fn get_year_duration(&self) -> Duration {
-        let year_start = self.get_year_start().unwrap();
-        let year_end = self.get_year_end().unwrap();
+    fn get_year_duration(&self) -> Option<Duration> {
+        let year_start = self.get_year_start()?;
+        let year_end = self.get_year_end()?;
 
-        year_end - year_start
+        Some(year_end - year_start)
     }
 
-    fn get_elapsed_duration(&self) -> Duration {
-        let year_start = self.get_year_start().unwrap();
+    fn get_elapsed_duration(&self) -> Option<Duration> {
+        let year_start = self.get_year_start()?;
 
-        self.datetime.date_naive() - year_start
+        Some(self.datetime.date_naive() - year_start)
     }
 
-    pub fn get(self) -> f64 {
-        let year_duration = self.get_year_duration().num_seconds();
-        let elapsed_duration = self.get_elapsed_duration().num_seconds();
+    pub fn get(self) -> Option<f64> {
+        let year_duration = self.get_year_duration()?.num_seconds();
+        let elapsed_duration = self.get_elapsed_duration()?.num_seconds();
 
-        elapsed_duration as f64 / year_duration as f64
+        Some(elapsed_duration as f64 / year_duration as f64)
     }
 }
 
 fn main() {
-    let year_progress = YearProgress::new(Utc::now()).get();
-
-    println!("Year Progress: {:.2}%", year_progress * 100.0);
+    match YearProgress::new(Utc::now()).get() {
+        Some(x) => println!("Year Progress: {:.2}%", x * 100.0),
+        None => println!("Something went wrong!")
+    };
 }
 
 #[cfg(test)]
 mod tests {
-    use chrono::TimeZone;
+    use chrono::{TimeZone, LocalResult};
     use super::*;
 
     #[test]
+    fn invalid_date() {
+        let invalid_date = match Utc.with_ymd_and_hms(2022, 13, 32, 25, 60, 61) {
+            LocalResult::Single(x) => x,
+            LocalResult::Ambiguous(x, _) => x,
+            LocalResult::None => return ()
+        };
+
+        assert_eq!(invalid_date.month(), 1);
+    }
+
+    #[test]
     fn past_year_progress() {
-        let year_progress = YearProgress::new(Utc.with_ymd_and_hms(2022, 12, 31, 0, 0, 0).unwrap()).get();
+        let year_progress = match YearProgress::new(Utc.with_ymd_and_hms(2022, 12, 31, 0, 0, 0).unwrap()).get() {
+            Some(x) => x,
+            None => 0.0
+        };
+
         assert_eq!(year_progress * 100.0, 100.00);
     }
 
     #[test]
     fn future_year_progress() {
-        let year_progress = YearProgress::new(Utc.with_ymd_and_hms(2030, 1, 1, 0, 0, 0).unwrap()).get();
+        let year_progress = match YearProgress::new(Utc.with_ymd_and_hms(2030, 1, 1, 0, 0, 0).unwrap()).get() {
+            Some(x) => x,
+            None => 0.0
+        };
+
         assert_eq!(year_progress * 100.0, 0.00);
     }
 }
