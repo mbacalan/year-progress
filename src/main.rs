9a04d9775d494e1147b5fa88b27462efc093a2ff
Muhammed Bacalan <muhammedbacalan@gmail.com>
2025-10-15 20:53:27 +0200

    refactor: Code cleanup using latest Rust feats & rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,80 @@
-use chrono::{Datelike, DateTime, Duration, Utc, NaiveDate};
+use chrono::{DateTime, Datelike, Duration, NaiveDate, Utc};
 
 struct YearProgress {
     datetime: DateTime<Utc>,
-    year: i32
+    year: i32,
 }
 
 impl YearProgress {
-    pub fn new(now: DateTime<Utc>) -> YearProgress {
-        YearProgress {
+    pub fn new(now: DateTime<Utc>) -> Self {
+        Self {
             datetime: now,
-            year: now.year()
+            year: now.year(),
         }
     }
 
-    fn get_year_start(&self) -> Option<NaiveDate> {
+    fn year_start(&self) -> Option<NaiveDate> {
         NaiveDate::from_ymd_opt(self.year, 1, 1)
     }
 
-    fn get_year_end(&self) -> Option<NaiveDate> {
+    fn year_end(&self) -> Option<NaiveDate> {
         NaiveDate::from_ymd_opt(self.year, 12, 31)
     }
 
-    fn get_year_duration(&self) -> Option<Duration> {
-        let year_start = self.get_year_start()?;
-        let year_end = self.get_year_end()?;
-
-        Some(year_end - year_start)
+    fn year_duration(&self) -> Option<Duration> {
+        self.year_start()
+            .zip(self.year_end())
+            .map(|(start, end)| end - start)
     }
 
-    fn get_elapsed_duration(&self) -> Option<Duration> {
-        let year_start = self.get_year_start()?;
-
-        Some(self.datetime.date_naive() - year_start)
+    fn elapsed_duration(&self) -> Option<Duration> {
+        self.year_start()
+            .map(|start| self.datetime.date_naive() - start)
     }
 
-    pub fn get(self) -> Option<f64> {
-        let year_duration = self.get_year_duration()?.num_seconds();
-        let elapsed_duration = self.get_elapsed_duration()?.num_seconds();
+    pub fn progress(&self) -> Option<f64> {
+        let year_duration = self.year_duration()?.num_seconds() as f64;
+        let elapsed_duration = self.elapsed_duration()?.num_seconds() as f64;
 
-        Some(elapsed_duration as f64 / year_duration as f64)
+        Some(elapsed_duration / year_duration)
     }
 }
 
 fn main() {
-    match YearProgress::new(Utc::now()).get() {
-        Some(x) => println!("Year Progress: {:.2}%", x * 100.0),
-        None => println!("Something went wrong!")
-    };
+    if let Some(progress) = YearProgress::new(Utc::now()).progress() {
+        println!("Year Progress: {:.2}%", progress * 100.0);
+    } else {
+        println!("Something went wrong!");
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use chrono::{TimeZone, LocalResult};
     use super::*;
+    use chrono::{LocalResult, TimeZone};
 
     #[test]
     fn invalid_date() {
-        let invalid_date = match Utc.with_ymd_and_hms(2022, 13, 32, 25, 60, 61) {
-            LocalResult::Single(x) => x,
-            LocalResult::Ambiguous(x, _) => x,
-            LocalResult::None => return ()
-        };
-
-        assert_eq!(invalid_date.month(), 1);
+        assert!(matches!(
+            Utc.with_ymd_and_hms(2022, 13, 32, 25, 60, 61),
+            LocalResult::None
+        ));
     }
 
     #[test]
     fn past_year_progress() {
-        let year_progress = match YearProgress::new(Utc.with_ymd_and_hms(2022, 12, 31, 0, 0, 0).unwrap()).get() {
-            Some(x) => x,
-            None => 0.0
-        };
+        let year_progress = YearProgress::new(Utc.with_ymd_and_hms(2022, 12, 31, 0, 0, 0).unwrap())
+            .progress()
+            .expect("a valid progress value");
 
         assert_eq!(year_progress * 100.0, 100.00);
     }
 
     #[test]
     fn future_year_progress() {
-        let year_progress = match YearProgress::new(Utc.with_ymd_and_hms(2030, 1, 1, 0, 0, 0).unwrap()).get() {
-            Some(x) => x,
-            None => 0.0
-        };
+        let year_progress = YearProgress::new(Utc.with_ymd_and_hms(2030, 1, 1, 0, 0, 0).unwrap())
+            .progress()
+            .expect("a valid progress value");
 
         assert_eq!(year_progress * 100.0, 0.00);
     }
